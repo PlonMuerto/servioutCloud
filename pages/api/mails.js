e28a5761750c712cc5abb85d51647ad6a8cd44c5
PlonMuerto <cannibalcommunity@protonmail.com>
2021-10-27 16:36:24 -0500

    Update mails.js

--- pages/api/mails.js
+++ pages/api/mails.js
@@ -4,22 +4,22 @@ const user = process.env.USEREMAIL
 const pass = process.env.PASSEMAIL 
 
 const transporter = nodemailer.createTransport({
-  "host":"mail.serviout.com.es",
-  "secure":true,
-  "port":465,
-  "auth":{
-    "type":"login",
-    "user":user,
-    "pass":pass
+  host:"mail.serviout.com.es",
+  secure:true,
+  port:465,
+  auth:{
+    type:"login",
+    user,
+    pass
   }
 
 });
 
 export default function handler(req, res) {
-  try{
+  
     let data = JSON.parse(req.body);
 
-    await transporter.sendMail({
+    transporter.sendMail({
       from:"administracion@serviout.com.es",
       to:user,
       subject:`cotizar servicio ${data.service}`,
@@ -34,12 +34,19 @@ export default function handler(req, res) {
         </p>
       </article>
 `
+    },function(error, info) {
+    	if (error) {  		
+        console.log("Error");
+        console.log(error);
+        res.status(500).json({confirm:false,err:JSON.stringify(error),error});
+    	} else {
+        
+    		res.status(200).json({ confirm:true, info,transporter});
+    	}
     })
-    res.status(200).json({ confirm:true, s,transporter,pass,user});
-  }catch(err){
-    console.log("Error");
-    console.log(err);
-    res.status(500).json({confirm:false,err:JSON.stringify(err),pass,user});
-  }
+    
+  
+    
+  
   
 }
